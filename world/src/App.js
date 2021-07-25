3a7270ded11df511b3cc0ec812630f2205555b6c
unknown <farizmammadov03@gmail.com>
2021-07-25 16:26:29 +0300

    Update

--- world/src/App.js
+++ world/src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import map from './countries/map.png'
+import Europe from './countries/Europe/europe.js'
 
-function App() {
+
+
+const App = () => {
   return (
-    <div className="App">
-      
+    <div className="map">
+      <svg viewBox="0 0 2457.3333 1266.6667">
+        <Europe />
+      </svg>
+      <img src={map} />
     </div>
   );
 }
