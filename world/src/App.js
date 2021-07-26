93feda5fc0b90b189d24a3ca6d0103f2206d3148
unknown <farizmammadov03@gmail.com>
2021-07-26 22:02:58 +0300

    Update

--- world/src/App.js
+++ world/src/App.js
@@ -6,12 +6,14 @@ import Europe from './countries/Europe/europe';
 
 const App = () => {
   return (
-    <div className="map">
-      <svg viewBox="0 0 2457.3333 1266.6667">
-        <Europe />
-      </svg>
-      <img src={map} alt="map"/>
-    </div>
+    
+      <div className="map">
+        <svg viewBox="0 0 2457.3333 1266.6667">
+          <Europe />
+        </svg>
+        <img src={map} alt="map" />
+      </div>
+    
   );
 }
 
