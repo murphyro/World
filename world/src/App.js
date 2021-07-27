8a2cd50745f4847486fe2a971309059b34ad7337
unknown <farizmammadov03@gmail.com>
2021-07-27 14:16:11 +0300

    Update

--- world/src/App.js
+++ world/src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './App.css';
 import map from './countries/map.png';
 import Europe from './countries/Europe/europe';
-import BorderBelarus from './countries/Europe/bel';
 import Belmap from './countries/Europe/belarus';
 import { BrowserRouter, Route } from 'react-router-dom';
 
@@ -13,11 +12,11 @@ const App = () => {
     <BrowserRouter>
       <div className="map">
         <svg viewBox="0 0 2457.3333 1266.6667">
-          <BorderBelarus />
-        
+          <Europe />               
         </svg>
+        <img src={map} />
+        <Route />
         <Route path="/belarus" component={Belmap}/>
-        <img src={map} alt="map" />
       </div>
     </BrowserRouter>
     
